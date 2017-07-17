40726d24e1a152476d45150b2238571ef07fdb6f
BraduCristina <cristina_bradu@mail.ru>
2017-07-17 13:30:29 +0300

    Fix icons/bg

--- bundle.js
+++ bundle.js
@@ -60,7 +60,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "93c6b997dd1839608706"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "0ce8424f6c688aa97de6"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
