0968591fbfebb1c35fb420c9f8e1bc5377e630bc
BraduCristina <cristina_bradu@mail.ru>
2017-07-19 15:05:18 +0300

    Fixing spaces

--- src/client/section/navbar.jsx
+++ src/client/section/navbar.jsx
@@ -4,10 +4,37 @@ import React from 'react'
 
 class Navbar extends React.Component {
 
-  scroll = (e) => {
-    // code
+  /*scroll = (e) => {
+    if (
+     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+     &&
+     location.hostname == this.hostname
+   ) {
+     // Figure out element to scroll to
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+     // Does a scroll target exist?
+     if (target.length) {
+       // Only prevent default if animation is actually gonna happen
+       event.preventDefault();
+       $('html, body').animate({
+         scrollTop: target.offset().top
+       }, 1000, function() {
+         // Callback after animation
+         // Must change focus!
+         var $target = $(target);
+         $target.focus();
+         if ($target.is(":focus")) { // Checking if the target was focused
+           return false;
+         } else {
+           $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+           $target.focus(); // Set focus again
+         };
+       });
+     }
+   }
   }
-
+*/
   render() {
     return (
       <div className="navbar navbar-default navbar-fixed-top" role="navigation" id="myNavbar">
